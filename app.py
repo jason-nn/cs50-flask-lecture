74430ab8130d9dd731ee15a675ddbd233d11586b
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-06 01:55:53 +0800

    Add simple greet page

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/greet')
+def greet():
     name = request.args.get('name') or 'World'
-    return render_template('index.html', name=name)
+    return render_template('greet.html', name=name)
