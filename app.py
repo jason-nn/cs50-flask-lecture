cbd975bb5176d50b1609d10f003dfcca337f920e
Jason Ho <jason.ho@obf.ateneo.edu>
2022-05-06 15:36:59 +0800

    Add support for get

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/greet', methods=['POST'])
+@app.route('/greet', methods=['GET', 'POST'])
 def greet():
     name = request.args.get('name') or 'World'
     return render_template('greet.html', name=name)
